613865a9be169aff0ba7609c0f129f41d53e92bb
ZSF <37545695+linktoword@users.noreply.github.com>
2022-03-05 12:10:55 +0800

    修改vite配置

--- vite.config.js
+++ vite.config.js
@@ -21,5 +21,24 @@ export default defineConfig({
 		alias: {
 			'@': path.resolve('src')
 		}
+	},
+	//配置代理
+	server: {
+		host: '0.0.0.0',
+		port: 3000,
+		open: true,
+		https: false,
+		proxy: {}
+	},
+
+	// 生产环境打包配置
+	//去除 console debugger
+	build: {
+		terserOptions: {
+			compress: {
+				drop_console: true,
+				drop_debugger: true
+			}
+		}
 	}
 })
